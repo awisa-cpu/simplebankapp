9c2272ae7bdc4743a1b67f0bd463f29db1885ebb
I_AM_AWISA_DESTINY <destinyawisa@gmail.com>
2022-11-13 13:12:05 +0100

    2.1.5

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,4 +1,3 @@
-import 'package:basicapp/constants/routes.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -35,119 +34,148 @@ class _LoginState extends State<Login> {
       appBar: AppBar(
         title: const Text('Welcome'),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 30,
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 20, right: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Account Number',
-                  style: TextStyle(
-                    color: Colors.grey.shade600,
-                  ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                TextField(
-                  decoration: const InputDecoration(
-                    hintText: 'Account Number',
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.horizontal(),
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.horizontal(),
-                    ),
-                  ),
-                  controller: _accNumber,
-                  autocorrect: false,
-                  enableSuggestions: false,
-                ),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
             ),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 20, right: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Password',
-                  style: TextStyle(
-                    color: Colors.grey.shade600,
-                  ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                TextField(
-                  decoration: const InputDecoration(
-                    hintText: 'Password',
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.horizontal(),
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.horizontal(),
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Account Number',
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
                     ),
                   ),
-                  controller: _password,
-                  autocorrect: false,
-                  enableSuggestions: false,
-                  obscureText: true,
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 20, right: 20),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text("Remember login"),
-                Expanded(child: Container()),
-                const Text("Login with Fingerprint")
-              ],
-            ),
-          ),
-          Container(
-            width: w * 0.9,
-            height: h * 0.15,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("img/loginbtn.png"),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  TextField(
+                    decoration: const InputDecoration(
+                      hintText: 'Account Number',
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.horizontal(),
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.horizontal(),
+                      ),
+                    ),
+                    controller: _accNumber,
+                    autocorrect: false,
+                    enableSuggestions: false,
+                  ),
+                ],
               ),
             ),
-            child: Center(
-              child: TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "SIGN IN",
-                  style: TextStyle(color: Colors.white),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Password',
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  TextField(
+                    decoration: const InputDecoration(
+                      hintText: 'Password',
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.horizontal(),
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.horizontal(),
+                      ),
+                    ),
+                    controller: _password,
+                    autocorrect: false,
+                    enableSuggestions: false,
+                    obscureText: true,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //there would be a checkbox implementation inplace of the TextButton
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Remember login",
+                      style: TextStyle(
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      //implement a showDialog pop up
+                    },
+                    child: const Text(
+                      "Login with Fingerprint",
+                      style: TextStyle(color: Colors.black54),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: w * 0.9,
+              height: h * 0.08,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("img/loginbtn.png"),
+                ),
+              ),
+              child: Center(
+                child: TextButton(
+                  onPressed: () {
+                    //implement a navigation for signIn to navigate to
+                  },
+                  child: const Text(
+                    "SIGN IN",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              "Forgot Password?",
-              style: TextStyle(color: Colors.black54),
-            ),
-          )
-        ],
+            TextButton(
+              onPressed: () {
+                //implement a navigation for forgot_Password? to navigate to
+              },
+              child: const Text(
+                "Forgot Password?",
+                style: TextStyle(
+                  color: Colors.black54,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
